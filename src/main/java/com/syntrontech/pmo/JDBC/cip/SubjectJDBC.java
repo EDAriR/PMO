3fe29a7a645408aafc2a734b69afa086b8f68564
ed_he <ed_he@syntrontech.com>
2018-07-16 00:02:16 +0800

    add subject JDBC

--- src/main/java/com/syntrontech/pmo/JDBC/cip/SubjectJDBC.java
+++ src/main/java/com/syntrontech/pmo/JDBC/cip/SubjectJDBC.java
@@ -1,23 +1,31 @@
 package com.syntrontech.pmo.JDBC.cip;
 
 import com.syntrontech.pmo.cip.model.Subject;
+import com.syntrontech.pmo.model.common.*;
 
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import java.sql.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.stream.Collectors;
 
 public class SubjectJDBC {
 
 
     private static final String GET_ALL_STMT = "SELECT * FROM subject ORDER BY sequence;";
     private static final String INSERT_STMT = "INSERT INTO subject " +
-            // TODO
-            "(id, name, mac_address, serial_number, unit_id, unit_name, tenant_id, status, " +
-            "createtime, createby, updatetime, updateby) "
+            "(sequence, id, name, gender, birthday, home_phone, address, ethnicity, " +
+            "personal_history, family_history, smoke, drink, chewing_areca, user_id, unit_id, unit_name," +
+            "tenant_id, createtime, createby, updatetime, updateby, status) "
+            + "VALUES (nextval('subject_sequence_seq'), ?, ?, ?, ?, ?, ?," +
+            "?, ?, ?, ?, ?, ?, ?, ?," +
+            "?, ?, ?, ?, ?, ?);";
 
-            + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+//    sequence, id, name, gender, birthday, home_phone, address, ethnicity
+//    personal_history, family_history, smoke, drink, chewing_areca, user_id, unit_id, unit_name
+//    tenant_id, createtime, createby, updatetime, updateby, status
 
 
 
@@ -28,23 +36,29 @@ public class SubjectJDBC {
 
         SubjectJDBC s = new SubjectJDBC();
         Date star_time = new Date();
-        List<HashMap<String, String>> ss = s.getAllSubjects(conn);
+        List<Subject> ss = s.getAllSubjects(conn);
         Date end_time = new Date();
 
         System.out.println("star_time:" + star_time.toInstant());
         System.out.println("end_time:" + end_time.toInstant());
         System.out.println("ss size:" + ss.size());
 
-        Subject subject = new Subject();
+        Subject subject = s.getSubject();
 
-        s.insertSubject(conn, subject);
+        System.out.println("unit_id");
+        System.out.println("unit_id");
+        System.out.println("unit_id");
+        System.out.println("unit_id");
+        System.out.println("unit_id");
+
+//        s.insertSubject(conn, subject);
 
     }
 
 
-    List<HashMap<String, String>> getAllSubjects(Connection conn){
+    List<Subject> getAllSubjects(Connection conn){
 
-        List<HashMap<String, String>> devices = new ArrayList<>();
+        List<Subject> devices = new ArrayList<>();
 
         PreparedStatement pstmt = null;
         ResultSet rs;
@@ -56,25 +70,66 @@ public class SubjectJDBC {
             if (rs != null) {
                 while (rs.next()) {
 
-                    HashMap<String, String> device = new HashMap<>();
-                    device.put("sequence", rs.getString("sequence"));
+                    Subject subject = new Subject();
 
-                    device.put("id" ,rs.getString("id"));
-                    device.put("name" , rs.getString("name"));
-                    device.put("mac_address" , rs.getString("mac_address"));
+//                    sequence, id, name, gender
+                    subject.setSequence(rs.getLong(""));
+                    subject.setId(rs.getString("id"));
+                    subject.setName(rs.getString("name"));
+                    GenderType gender = rs.getString("gender") == null ? GenderType.valueOf(rs.getString("gender")) : null;
+                    subject.setGender(gender);
 
-                    device.put("serial_number" , rs.getString("serial_number"));
-                    device.put("unit_id" , rs.getString("unit_id"));
-                    device.put("unit_name" , rs.getString("unit_name"));
-                    device.put("tenant_id" , rs.getString("tenant_id"));
-                    device.put("createtime" , rs.getString("createtime"));
-                    device.put("createby" , rs.getString("createby"));
-                    device.put("updatetime" , rs.getString("updatetime"));
-                    device.put("updateby" , rs.getString("updateby"));
-                    device.put("status" , rs.getString("status"));
+//                    birthday, home_phone, address, ethnicity
+                    subject.setBirthday(rs.getDate("birthday"));
+                    subject.setHomePhone(rs.getString("home_phone"));
+                    subject.setAddress(rs.getString("address"));
 
-//                    System.out.println("device:" + device);
-                    devices.add(device);
+                    EthnicityType ethnicityType = rs.getString("gender") == null ? EthnicityType.valueOf(rs.getString("gender")) : null;
+                    subject.setEthnicity(ethnicityType);
+
+//                    personal_history, family_history, smoke, drink
+
+                    List<PersonalHistoryType> personal_history = Arrays.asList((String[])rs.getArray("personal_history").getArray())
+                            .stream()
+                            .map(ph -> PersonalHistoryType.valueOf(ph))
+                            .collect(Collectors.toList());
+
+                    subject.setPersonalHistory(personal_history);
+
+                    List<FamilyHistoryType> family_history = Arrays.asList((String[])rs.getArray("family_history").getArray())
+                            .stream()
+                            .map(ph -> FamilyHistoryType.valueOf(ph))
+                            .collect(Collectors.toList());
+                    subject.setFamilyHistory(family_history);
+
+                    SmokeType smoke = rs.getString("smoke") == null ? SmokeType.valueOf(rs.getString("smoke")) : null;
+                    subject.setSmoke(smoke);
+
+                    DrinkType drink = rs.getString("drink") == null ? DrinkType.valueOf(rs.getString("drink")) : null;
+                    subject.setDrink(drink);
+
+//                    chewing_areca, user_id, unit_id, unit_name
+                    ChewingArecaType chewingAreca = rs.getString("chewing_areca") == null ?
+                            ChewingArecaType.valueOf(rs.getString("chewing_areca")) : null;
+                    subject.setChewingAreca(chewingAreca);
+
+                    subject.setUserId(rs.getString("user_id"));
+                    subject.setUnitId(rs.getString("unit_id"));
+                    subject.setUnitName(rs.getString("unit_name"));
+
+//                    tenant_id, createtime, createby, updatetime
+                    subject.setTenantId(rs.getString("tenant_id"));
+                    subject.setCreateTime(rs.getDate("createtime"));
+                    subject.setCreateBy(rs.getString("createby"));
+                    subject.setUpdateTime(rs.getDate("updatetime"));
+
+//                    updateby, status
+                    subject.setUpdateBy(rs.getString("updateby"));
+                    ModelStatus status = rs.getString("status") == null ? ModelStatus.valueOf(rs.getString("status")) : null;
+                    subject.setStatus(status);
+
+                    System.out.println("subject:" + subject);
+                    devices.add(subject);
                 }
             }
         } catch (SQLException e) {
@@ -93,15 +148,39 @@ public class SubjectJDBC {
         try {
             pstmt = conn.prepareStatement(INSERT_STMT);
 
-            Statement stgfid = conn.prepareStatement("SELECT last_value FROM forums_id_seq");
-            ResultSet rsgfid = stgfid.executeQuery("");
-            rsgfid.next();
-            int forumId = rsgfid.getInt(1);
-            rsgfid.close();
-            stgfid.close();
+            //    sequence, id, name, gender, birthday,
+            pstmt.setString(1, subject.getId());
+            pstmt.setString(2,subject.getName());
+            pstmt.setString(3, subject.getGender().toString());
+            pstmt.setDate(4,new java.sql.Date(subject.getBirthday().getTime()));
 
-            pstmt.setLong(1, forumId);
+            //    home_phone, address, ethnicity
+            pstmt.setString(5, subject.getHomePhone());
+            pstmt.setString(6, subject.getAddress());
+            pstmt.setString(7, subject.getEthnicity().toString());
 
+            //    personal_history, family_history, smoke, drink,
+            Array personalHistory = conn.createArrayOf("varchar", subject.getPersonalHistory());
+            pstmt.setArray(8, personalHistory);
+
+            Array familyHistory = conn.createArrayOf("varchar", subject.getFamilyHistory() );
+            pstmt.setArray(9, familyHistory);
+            pstmt.setString(10, subject.getSmoke().toString());
+            pstmt.setString(11, subject.getDrink().toString());
+
+            // chewing_areca, user_id, unit_id, unit_name
+            pstmt.setString(12, subject.getChewingAreca().toString());
+            pstmt.setString(13, subject.getUserId());
+            pstmt.setString(14, subject.getUnitId());
+            pstmt.setString(15, subject.getUnitName());
+
+            //    tenant_id, createtime, createby, updatetime, updateby, status
+            pstmt.setString(16, subject.getTenantId());
+            pstmt.setDate(17, new java.sql.Date(subject.getCreateTime().getTime()));
+            pstmt.setString(18, subject.getCreateBy());
+            pstmt.setDate(19, new java.sql.Date(subject.getUpdateTime().getTime()));
+            pstmt.setString(20, subject.getUpdateBy());
+            pstmt.setString(21, subject.getStatus().toString());
 
             pstmt.executeUpdate();
             System.out.println("create successful ==> " + subject);
@@ -109,6 +188,50 @@ public class SubjectJDBC {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    Subject getSubject() {
+
+        Subject subject = new Subject();
+
+
+
+
+//        sequence, id, name, gender
+        subject.setId("SubjectJDBCTest");
+        subject.setName("SubjectJDBCTest");
+        subject.setGender(GenderType.FEMALE);
+
+//        birthday, home_phone, address, ethnicity
+        subject.setBirthday(new Date(666));
+        subject.setHomePhone("home_phone");
+        subject.setAddress("address");
+
+        subject.setEthnicity(EthnicityType.HAN);
+
+//        personal_history, family_history, smoke, drink
+        subject.setPersonalHistory(Arrays.asList(PersonalHistoryType.HYPERTENSION));
+        subject.setFamilyHistory(Arrays.asList(FamilyHistoryType.HYPERTENSION));
+        subject.setSmoke(SmokeType.SOCIALITY);
+        subject.setDrink(DrinkType.OFTEN);
+
+//        chewing_areca, user_id, unit_id, unit_name
+        subject.setChewingAreca(ChewingArecaType.OFTEN);
+
+        subject.setUserId("systemAdmin");
+        subject.setUnitId("unit_id");
+        subject.setUnitName("unit_name");
+
+//        tenant_id, createtime, createby, updatetime
+        subject.setTenantId("tenant_id");
+        subject.setCreateTime(new Date());
+        subject.setCreateBy("systemAdmin");
+        subject.setUpdateTime(new Date());
+
+//                    updateby, status
+        subject.setUpdateBy("systemAdmin");
+        subject.setStatus(ModelStatus.ENABLED);
+        return subject;
     }
 
 }
