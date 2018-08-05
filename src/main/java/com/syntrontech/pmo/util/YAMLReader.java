7835f99df65e99864f0250b0ecb7a5824a9dea4b
ed_he <ed_he@syntrontech.com>
2018-08-05 21:32:37 +0800

    yaml fail

---
+++ src/main/java/com/syntrontech/pmo/util/YAMLReader.java
@@ -0,0 +1,25 @@
+package com.syntrontech.pmo.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class YAMLReader {
+
+    public static void main(String[] args) throws IOException {
+
+//        Yaml yaml = new Yaml();
+//        InputStream in = YAMLReader.class.getResourceAsStream("/setting.yml");
+//        Object obj = yaml.load(in);
+//        System.out.println("Loaded object type:" + obj.getClass());
+//        System.out.println(obj);
+
+    }
+
+    public void getSetting(){
+
+//        Yaml yaml = new Yaml();
+    }
+}
