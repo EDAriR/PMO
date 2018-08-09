dfe8e1d707745c4a5ece213249e16de0cef8050d
ed_he <ed_he@syntrontech.com>
2018-08-09 17:00:15 +0800

    yml 50%

--- src/main/java/com/syntrontech/pmo/util/YAMLReader.java
+++ src/main/java/com/syntrontech/pmo/util/YAMLReader.java
@@ -5,17 +5,31 @@ import org.yaml.snakeyaml.Yaml;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class YAMLReader {
 
     public static void main(String[] args) throws IOException {
 
-//        Yaml yaml = new Yaml();
-//        InputStream in = YAMLReader.class.getResourceAsStream("/setting.yml");
-//        Object obj = yaml.load(in);
-//        System.out.println("Loaded object type:" + obj.getClass());
-//        System.out.println(obj);
+        Yaml yaml = new Yaml();
+        InputStream in = YAMLReader.class.getResourceAsStream("/setting.yml");
+        Object obj = yaml.load(in);
+        System.out.println("Loaded object type:" + obj.getClass());
+        System.out.println(obj instanceof LinkedHashMap);
 
+        Map map;
+        if(obj instanceof LinkedHashMap) {
+            map = (LinkedHashMap) obj;
+        }else {
+            map = new LinkedHashMap();
+        }
+        Map pg;
+        if(map.get("postgresql") instanceof LinkedHashMap)
+            pg = (LinkedHashMap)map.get("postgresql");
+        else
+            pg = new LinkedHashMap();
+        System.out.println(pg.get("driver"));
     }
 
     public void getSetting(){
